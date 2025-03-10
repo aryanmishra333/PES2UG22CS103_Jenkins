aed919bc71a4b81fdb55a6e77f0d870725caa274
Aryan Mishra <aryangungun4187@gmail.com>
2025-03-10 22:08:49 +0530

    major commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<< "Changes and commit to check if Jenkins is setup properly";
+  cout<< "Changes and commit to check if Jenkins is setup properly \n";
   return 0;
 }
